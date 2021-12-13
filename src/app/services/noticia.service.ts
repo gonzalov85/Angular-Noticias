41e823045077b951ccfeff7918285614612ba010
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-13 18:38:17 -0300

    adding getNoticia http service

--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -1,15 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiaService {
 
-  getNoticias(parametros:any){
+  
+
+  constructor(private http: HttpClient) { }
+
+  getNoticias(parametros:any): Observable<any>{
     const URL = 'https://newsapi.org/v2/top-headlines?country='
     + parametros.pais +'&category=' + parametros.categoria + '&apiKey=3cc2d474539c498b951f823fc37b1127';
+
+    return this.http.get(URL);
     
   }
-
-  constructor() { }
 }
