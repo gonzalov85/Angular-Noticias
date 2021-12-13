300372625030711b5c316e98d57bb8b24738132c
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-13 20:08:44 -0300

    spinner and cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'noticias';
 
+  listNoticias: any[] = [];
+
+  loading = false;
+
+  constructor(private _noticiaService: NoticiaService){
+
+  }
+
   buscarNoticia(parametros:any){
-    console.log("soy el padre");
-    console.log(parametros);
+
+    setTimeout(() => {
+      this.loading = true;
+      this.listNoticias = [];
+      this._noticiaService.getNoticias(parametros).subscribe(data =>{
+        this.listNoticias = data.articles;
+        this.loading = false;
+      }, error => {
+        console.log(error)
+        this.loading = false;
+      })
+    },1000);
   }
 }
